bad2102449fd120c7e8cf79317b8e249b57a1539
productdevbook <hi@productdevbook.com>
2023-10-20 14:22:11 +0300

    feat: upload

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 import { getInput } from '@actions/core'
 
-const nameToGreet = getInput('nitro_deploy_token')
+import { uploadFile } from 'nitro-deploy/upload'
 
-console.warn(`Hello ${nameToGreet}!`)
+const token = getInput('nitro_deploy_token')
+const buildCommand = getInput('nitro_deploy_build_command')
+const exportFolder = getInput('nitro_deploy_export_folder')
+
+await uploadFile({
+  token,
+  buildCommand: buildCommand || 'npm run build',
+  exportFolder: exportFolder || '.output',
+})
